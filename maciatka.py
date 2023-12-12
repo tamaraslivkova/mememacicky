696b04978bc613b0cc98090cd3c8dc48f6c231b0
emak7696 <145448940+emak7696@users.noreply.github.com>
2023-12-12 12:14:29 +0100

    visuals of the page

--- maciatka.py
+++ maciatka.py
@@ -9,6 +9,10 @@ app = Flask(__name__)
 # IMAGE GENERATION
 # --------------------
 
+# AHOJ Tamka!
+# ahoj emka/kamil
+# ahojte kamosi
+
 # OpenAI image generator
 # Register to openAI using personal API KEY
 @app.route('/result',methods=["POST"])
@@ -16,7 +20,7 @@ def generateimage():
     data = request.form
     prompt = f"""You are a realistic cat photo generator.\n Generate a picture of a {data['feel']} {data['color']} cat 
     wearing a {data['hat']} on a {data['background']} background with a {data['food']} and on the cat's right is a 
-    {data['kitten']} kitten with {data['gift']} flower and on its left is a {data['coffee']}.\n"""
+    {data['vinea']} kitten with {data['gift']} flower and on its left is a {data['coffee']}.\n"""
     openai.api_key = os.getenv("OPENAI_KEY")
     response = openai.Image.create(
         prompt=prompt,
